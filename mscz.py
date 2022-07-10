87dafc1418c87f01a16242614a6c90894da6460f
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2022-07-10 17:20:07 +0200

    documentation

--- mscz.py
+++ mscz.py
@@ -1,64 +1,74 @@
 from lxml import etree
 class Mscz:
+    """ Class to parse MSCZ (MuseScore) data files """
     Clefs = ["G", "F"]
-    def __init__ (self, fp):
-        self.root = etree.parse(fp)
-    
-    def __handle (self, tag, r=None):
-        """ 
+    def __init__ (self, filepath):
+        self.root = etree.parse(filepath)
+
+    def __handle (self, tag, root=None):
+        """
         Returns handle pointer to child matching given tag
         """
-        if r is None:
-            r = self.root
-        for child in r.iter():
+        if root is None:
+            root = self.root
+        for child in root.iter():
             if child.tag == tag:
                 return child
         return None
 
     def score (self):
-        """ 
-        Returns handle to Score marker 
+        """
+        Returns handle to Score marker
         """
         return self.__handle("Score")
-    
+
     def part (self):
-        return self.__handle("Part", r=self.score())
+        """
+        Returns hande to Part marker
+        """
+        return self.__handle("Part", root=self.score())
 
     def style (self):
-        """ 
-        Returns handle to Style marker 
         """
-        return self.__handle("Style", r=self.score())
-    
+        Returns handle to Style marker
+        """
+        return self.__handle("Style", root=self.score())
+
     def page_layout (self):
-        """ 
-        Returns handle to page layout 
         """
-        return self.__handle("page-layout", r=self.style())
-    
+        Returns handle to page layout
+        """
+        return self.__handle("page-layout", root=self.style())
+
     def page_geometry (self):
-        """ 
-        Returns Page geometry as (Height, Width) tuple 
         """
-        (h,w) = (None,None)
-        for c in self.page_layout():
-            if c.tag == "page-height":
-                h = float(c.text)
-            elif c.tag == "page-width":
-                w = float(c.text)
-        return (h,w)
+        Returns Page geometry as (Height, Width) tuple
+        """
+        (height, width) = (None,None)
+        for child in self.page_layout():
+            if child.tag == "page-height":
+                height = float(child.text)
+            elif child.tag == "page-width":
+                width = float(child.text)
+        return (height, width)
 
     def page_geometry_advanced (self):
-        """ 
-        Returns Page geometry as complex dict, with margins 
         """
-        (h, w) = self.page_geometry()
-        for child in self.__handle("page-margins", r=self.page_layout()):
+        Returns Page geometry as complex dict, with margins
+        """
+        geo = {}
+        (height, width) = self.page_geometry()
+        geo['heigth'] = height
+        geo['width'] = width
+        geo['even'] = {}
+        geo['odd'] = {}
+        for child in self.__handle("page-margins", root=self.page_layout()):
             print(child.tag) #TODO
+        return geo
 
     def __len__ (self):
-        """ 
-        Returns number of staffs contained in self 
+        """
+        Returns number of staffs contained in self
         """
         i = 0
         for child in self.part():
@@ -66,22 +76,23 @@ class Mscz:
                 i += 1
         return i
 
-    def staff (self, id):
-        """ 
-        Returns handle to desired track 
+    def staff (self, staffid):
+        """
+        Returns handle to desired track
         """
         for child in self.part():
             if child.tag == "Staff":
                 if "id" in child.attribute:
-                    if child.attribute["id"] == id:
+                    if child.attribute["id"] == staffid:
                         return child
+        return None
 
     def total_measures (self):
         """
         Returns total number of measures
         """
-        m = 0
-        for c in self.score().iter():
-            if c.tag == "Measure":
-                m += 1
-        return m // len(self) #TODO a revoir
+        total = 0
+        for child in self.score().iter():
+            if child.tag == "Measure":
+                total += 1
+        return total // len(self) #TODO a revoir
