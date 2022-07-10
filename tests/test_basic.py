16b0ae3995de2464b3e80b7210cf383d301c1514
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2022-07-10 18:11:32 +0200

    basic tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,19 +1,15 @@
 #! /usr/bin/env python3
-import os
 import sys
 from mscz import Mscz
 
 def test_basic():
-    print(os.listdir())
-    m = Mscz("tests/Space_Oddity.mscx") 
-    print(m.score())
-    print(m.style())
-    print(m.page_layout())
-    print("Geometry", m.page_geometry())
+    m = Mscz("tests/example.mscx") 
+    assert m.score() is not None
+    assert m.style() is not None
+    assert m.page_layout() is not None
+    (h, w) = m.page_geometry()
+    assert h == 1683.36
+    assert w == 1190.88
     print("Number of staffs", len(m))
     #print(m.page_geometry_advanced())
-    print(m.staff,"1")
-    print(m.total_measures())
-
-if __name__ == "__main__":
-    main(sys.argv[1])
+    assert m.total_measures() == 58
